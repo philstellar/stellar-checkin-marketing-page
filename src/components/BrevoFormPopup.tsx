f6b721198f11586d84ec957de75c62a3202b3fcb
philstellar <philipp@stellar-trust.com>
2025-04-15 14:41:49 +0200

    Update BrevoFormPopup.tsx

--- src/components/BrevoFormPopup.tsx
+++ src/components/BrevoFormPopup.tsx
@@ -52,7 +52,7 @@ const BrevoFormPopup = ({ isOpen, onClose }: BrevoFormPopupProps) => {
                   <path d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z" />
                 </svg>
                 <span className="sib-form-message-panel__inner-text">
-                  Bitte nochmak versuchen
+                  Bitte nochmal versuchen
                 </span>
               </div>
             </div>
@@ -63,7 +63,7 @@ const BrevoFormPopup = ({ isOpen, onClose }: BrevoFormPopupProps) => {
                   <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z" />
                 </svg>
                 <span className="sib-form-message-panel__inner-text">
-                  Das war der erste Schritt. Wir melden uns sehr bald bei dir
+                  Das war der erste Schritt. Wir melden uns zeitnah bei dir
                 </span>
               </div>
             </div>
@@ -77,7 +77,7 @@ const BrevoFormPopup = ({ isOpen, onClose }: BrevoFormPopupProps) => {
               <form id="sib-form" method="POST" action="https://sibforms.com/serve/MUIFAJ_DcxJlvRD3psRVx2nDGNe2zYivbNrzs3_y_FJfTjM-wKiJDkVmebrGnMcOU4bBTRIOoCStBnk0pI2mwibEI-QLGkze73zvN0mzQlpC5p7yGWlIRc7S4WpRnMOknyl-wanGlvGGmUMnEa0zbSsKEL-ygR43D1uTXvrZYkF_Olmhv2vo5PUDda4qusHFvYGmFoq2oi49A8G3" data-type="subscription">
                 <div style={{ padding: "8px 0" }}>
                   <div className="sib-form-block text-center" style={{ fontSize: "32px", fontWeight: 700, fontFamily: "Helvetica, sans-serif", color: "#3C4858", backgroundColor: "transparent" }}>
-                    <p>Infos anfordern und Demo vereinbaren</p>
+                    <p>Fülle das Formular aus und erstelle dein kostenloses Konto</p>
                   </div>
                 </div>
                 <div style={{ padding: "8px 0" }}>
@@ -100,7 +100,7 @@ const BrevoFormPopup = ({ isOpen, onClose }: BrevoFormPopupProps) => {
                       </label>
                       <div className="flex justify-center w-full">
                         <label className="entry__specification text-center w-3/4" style={{ fontSize: "12px", fontFamily: "Helvetica, sans-serif", color: "#8390A4" }}>
-                          Wir melden uns innerhalb von kürzester Zeit bei dir für weiter Infos zu unserem Produkt und einer Demo
+                          In Kürze erhältst du von uns weitere Infos und den Registrierungslink
                         </label>
                       </div>
                     </div>
@@ -111,7 +111,7 @@ const BrevoFormPopup = ({ isOpen, onClose }: BrevoFormPopupProps) => {
 <button className="sib-form-block__button sib-form-block__button-with-loader" style={{ fontSize: "16px", fontWeight: 700, fontFamily: "Helvetica, sans-serif", color: "#FFFFFF", backgroundColor: "#a4c309", borderRadius: "3px", borderWidth: "0px", padding: "8px 16px" }} form="sib-form" type="submit"   onClick={() => gtag_report_conversion()}>                      <svg className="icon clickable__icon progress-indicator__icon sib-hide-loader-icon" viewBox="0 0 512 512">
                         <path d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z" />
                       </svg>
-                      Hier klicken für den besten Online Checkin
+                      Jetzt registrieren
                     </button>
                   </div>
                 </div>
